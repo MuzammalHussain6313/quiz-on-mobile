e080ac90fc729b6867d031397c4d4df6aaa11d5c
Muzammil Hussain <51987198+MuzammalHussain6313@users.noreply.github.com>
2021-02-10 02:16:54 +0500

    23456ertyui

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/qob'));
+app.use(express.static(__dirname + '/dist/quiz-on-mobile'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/qob/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/quiz-on-mobile/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
